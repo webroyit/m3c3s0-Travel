6af457fad78b3e52de9e3f9596e8a91ea3ef7828
royweb <xytank@gmail.com>
2020-03-31 13:26:45 -0400

    Use M.Slider.init() for the slider to work

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
 const sideNav = document.querySelector(".sidenav");
 // use Materialize to show the sidenav
 M.Sidenav.init(sideNav, {});
+
+// slider
+const slider = document.querySelector(".slider");
+M.Slider.init(slider, {
+    indicators: false,      // hide the dots
+    height: 500,
+    transition: 500,        // speed of the transition
+    interval: 6000          // time for a image to switch
+})
