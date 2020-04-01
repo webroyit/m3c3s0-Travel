8c4f8791b01581d468a1e33c3dff25ab0110936f
royweb <xytank@gmail.com>
2020-04-01 11:53:35 -0400

    Use M.Autocomplete.init() for autocomplete to work

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ M.Slider.init(slider, {
     transition: 500,        // speed of the transition
     interval: 5000          // time for a image to switch
 })
+
+// autocomplete
+const ac = document.querySelector(".autocomplete");
+M.Autocomplete.init(ac, {
+    data: {
+        "New York": null,
+        "Paris": null,
+        "Rome": null,
+        "Cancun": null,
+        "London": null,
+        "Miami": null,
+        "Orlando": null,
+        "San Francisco": null,
+    }
+})
